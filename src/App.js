24404ff316ca4e1614b9383e0dd632c6e729185a
wa-wa00 <walid.akaraniti@gmail.com>
2026-07-20 10:54:09 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Header } from './func-components';
 import { Content } from './func-components';
+import { Footer } from './func-components';
 
 function App() {
   return (
