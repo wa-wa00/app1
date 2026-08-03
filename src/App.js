d3ddda9435cd5a4de4ce541f2bf16e5360b24ed9
wa-wa00 <walid.akaraniti@gmail.com>
2026-08-03 10:14:08 +0700

    calculate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Header,Content,Footer } from './func-components';
 import Calendar from './class-component';
+import {Calculator2} from './calculator'
+
 
 function App() {
   return (
     <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-
-      <Footer/>
+      <Calculator2/>
     </>
   )
 }
