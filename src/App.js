97eb42edd9b9a9f0ff6bd2b93f5e79f9182ec175
wa-wa00 <walid.akaraniti@gmail.com>
2026-08-10 10:48:45 +0700

    RefsArray

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Calendar from './class-component';
 import {Calculator2} from './calculator'
 import {EventData2} from './event-data'
 import RefsFunc from './refs-func'
+import RefsArray from './refs-array'
 
-function App() {
-  return <RefsFunc/>
+export default function App() {
+    return <RefsArray />
 }
-export default App;
 
